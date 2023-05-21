df7e2d132c856a08239082fa525805fca09749d7
BK-jp <devjp3809@gmail.com>
2023-05-21 16:45:31 +0900

    first commit

---
+++ backend/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello")
+	})
+
+	app.Get("/:value", func(c *fiber.Ctx) error {
+		return c.SendString("value : " + c.Params("value"))
+	})
+
+	app.Listen(":3000")
+}
